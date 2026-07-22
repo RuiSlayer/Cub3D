b6c39ba69ea3fde6e6708582a64f3daac792ac0d
Francisco Gameiro <kiko@MacBook-Pro-de-Francisco.local>
2026-07-22 23:46:28 +0100

    Joined parsing and texturing

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slayer <slayer@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fgameiro <fgameiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 19:06:02 by slayer            #+#    #+#             */
-/*   Updated: 2026/07/21 19:07:04 by slayer           ###   ########.fr       */
+/*   Updated: 2026/07/22 23:35:09 by fgameiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	main(int argc, char const **argv)
 		return (ft_dprintf(2, "Error\nA map should be pass as argument\n"));
 	init_cub(&cub);
 	load_map(argv[1], &cub);
-	if (init_debug_scene(&cub))
-		return (free_cub(&cub), 1);
+	print_map(&cub);
+	player_init(&cub);
 	if (init_mlx(&cub))
 		return (free_cub(&cub), 1);
 	setup_hooks(&cub);
